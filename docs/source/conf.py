1a3153457bb0d58d658683babd3a5fed505b9ba6
lgaalves <lgaalves@northwestern.edu>
2022-04-12 17:12:33 -0500

    correct version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Luiz G. A. Alves'
 author = 'Luiz G. A. Alves'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
